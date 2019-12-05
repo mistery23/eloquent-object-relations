5ab1a014b5e65cba952801ca424cade8765bcd85
sviatoslav <bugatis15@gmail.com>
2019-12-05 14:00:51 +0200

    fix

--- src/Classes/HasMany.php
+++ src/Classes/HasMany.php
@@ -30,4 +30,31 @@ class HasMany extends \Illuminate\Database\Eloquent\Relations\HasMany
             }
         )->all();
     }
+
+    /**
+     * Match the eagerly loaded results to their many parents.
+     *
+     * @param  array   $models
+     * @param  \Illuminate\Database\Eloquent\Collection  $results
+     * @param  string  $relation
+     * @param  string  $type
+     * @return array
+     */
+    protected function matchOneOrMany(array $models, Collection $results, $relation, $type)
+    {
+        $dictionary = $this->buildDictionary($results);
+
+        // Once we have the dictionary we can simply spin through the parent models to
+        // link them up with their children using the keyed dictionary to make the
+        // matching very convenient and easy work. Then we'll just return them.
+        foreach ($models as $model) {
+            if (isset($dictionary[$key = (string)$model->getAttribute($this->localKey)])) {
+                $model->setRelation(
+                    $relation, $this->getRelationValue($dictionary, $key, $type)
+                );
+            }
+        }
+
+        return $models;
+    }
 }
